d5ee3bf3d54d3ca50281ac9bae3847080bc89b67
gunturborneo31 <borneomguntur0@gmail.com>
2026-06-20 13:45:56 +0800

    push

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title inertia>SIMEVLAP 2.0</title>
         @routes
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+@vite('resources/js/app.js')
         @inertiaHead
     </head>
     <body class="font-sans antialiased bg-gray-50">
